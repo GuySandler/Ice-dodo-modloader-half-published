3bfd85048b4cf16e092da529b1f03afdcca0964f
Guy Sandler <guy@sandler.co.il>
2024-07-01 07:35:34 -0700

    commit 20

--- boot.js
+++ boot.js
@@ -3,8 +3,8 @@ const mapScript = document.querySelector('#map-script').innerHTML;
 eval(mapScript);
 
 // making the meshes
-BABYLON.Tags.AddTagsTo(platform, "mesh");
-BABYLON.Tags.AddTagsTo(cone, "mesh");
-BABYLON.Tags.AddTagsTo(sphere, "mesh");
-BABYLON.Tags.AddTagsTo(ending, "mesh");
+BABYLON.Tags.AddTagsTo(platform, "mesh platform");
+BABYLON.Tags.AddTagsTo(cone, "mesh cone");
+BABYLON.Tags.AddTagsTo(sphere, "mesh sphere");
+BABYLON.Tags.AddTagsTo(ending, "mesh ending");
 BABYLON.Tags.AddTagsTo(mesh, "mesh");
