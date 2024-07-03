6b97d87f763afc884018fdf17937dc79c818e679
Guy Sandler <guy@sandler.co.il>
2024-07-02 22:52:18 -0700

    commit 21

--- boot.js
+++ boot.js
@@ -8,3 +8,13 @@ BABYLON.Tags.AddTagsTo(cone, "mesh cone");
 BABYLON.Tags.AddTagsTo(sphere, "mesh sphere");
 BABYLON.Tags.AddTagsTo(ending, "mesh ending");
 BABYLON.Tags.AddTagsTo(mesh, "mesh");
+
+if (!document.getElementById("PracticeMode").checked) {
+    player.position = new BABYLON.Vector3(map.spawn[0],map.spawn[1],map.spawn[2]);
+    player.rotation = new BABYLON.Vector3(0,0,0);
+}
+else {
+    player.position = new BABYLON.Vector3(document.getElementById("PracticeX").value,document.getElementById("PracticeY").value,document.getElementById("PracticeZ").value);
+    player.rotation = new BABYLON.Vector3(0,0,0);
+    // rotation = document.getElementById("PracticeR").value;
+}
