974da68da87abe2d4b0694ff9e0e06df28fd86a9
Guy Sandler <guy@sandler.co.il>
2024-07-04 01:35:30 -0700

    commit 23

--- boot.js
+++ boot.js
@@ -18,3 +18,27 @@ else {
     player.rotation = new BABYLON.Vector3(0,0,0);
     // rotation = document.getElementById("PracticeR").value;
 }
+
+// ...
+decorateCustomLevel: function(meshes, platforms, cones, ends) {
+    // let meshes = scene.getMeshesByTags("mesh");
+    // let platforms = scene.getMeshesByTags("platform");
+    // let cones = scene.getMeshesByTags("cone");
+    // let ends = scene.getMeshesByTags("end");
+
+    let pmat = new BABYLON.StandardMaterial("pmat", scene);
+    pmat.diffuseTexture = new BABYLON.Texture("assets/textures/bright.png", scene);
+    pmat.diffuseTexture.uScale = pmat.diffuseTexture.vScale = 1.0;
+    pmat.backFaceCulling = false;
+    pmat.freeze();
+
+    for(let i = 0;i<platforms.length;i++){
+        platforms[i].material = pmat;
+    }
+    for(let i = 0;i<cones.length;i++){
+        cones[i].material = this.rgba_mat(235,50,50,1.0);
+    }
+    for(let i = 0;i<ends.length;i++){
+        ends[i].material = this.rgba_mat(0, 240, 0, 0.8);
+    }
+}
